07de00da31b06c2e00852524ed49528a45eb43b2
hp <hp@DESKTOP-FFPPH0C>
2022-09-13 15:42:47 +0530

    add values

--- jntu_calc_application/src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ jntu_calc_application/src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -3,6 +3,7 @@ package ETA_SB.jntu_calc_application;
 public class Calculator {
 	public double doAdd(double num1, double num2) {
 		return num1 + num2;
+		//intitial
 	}
 
 	public double doSub(double num1, double num2) {
